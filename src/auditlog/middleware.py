6e53a8e1082a92d0719a29f4824b3c75026a37ee
David Bramwell <david.bramwell@wearesponge.com>
2021-07-28 11:16:59 +0100

    Fix logs being attributed to wrong user

--- src/auditlog/middleware.py
+++ src/auditlog/middleware.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 from django.conf import settings
 from django.db.models.signals import pre_save
@@ -6,6 +7,7 @@ from django.utils.functional import curry
 from django.db.models.loading import get_model
 from auditlog.models import LogEntry
 
+threadlocal = threading.local()
 
 class AuditlogMiddleware(object):
     """
@@ -18,37 +20,40 @@ class AuditlogMiddleware(object):
         Gets the current user from the request and prepares and connects a signal receiver with the user already
         attached to it.
         """
+        # Initialize thread local storage
+        threadlocal.auditlog = {
+            'signal_duid': (self.__class__, time.time())
+        }
+
         if hasattr(request, 'user') and hasattr(request.user, 'is_authenticated') and request.user.is_authenticated():
-            user = request.user
-            request.auditlog_ts = time.time()
-            set_actor = curry(self.set_actor, user)
-            pre_save.connect(set_actor, sender=LogEntry, dispatch_uid=(self.__class__, request.auditlog_ts), weak=False)
+            set_actor = curry(self.set_actor, user=request.user, signal_duid=threadlocal.auditlog['signal_duid'])
+            pre_save.connect(set_actor, sender=LogEntry, dispatch_uid=threadlocal.auditlog['signal_duid'], weak=False)
 
     def process_response(self, request, response):
         """
         Disconnects the signal receiver to prevent it from staying active.
         """
-        # Disconnecting the signal receiver is required because it will not be garbage collected (non-weak reference)
-        if hasattr(request, 'auditlog_ts'):
-            pre_save.disconnect(sender=LogEntry, dispatch_uid=(self.__class__, request.auditlog_ts))
-
+        if hasattr(threadlocal, 'auditlog'):
+            pre_save.disconnect(sender=LogEntry, dispatch_uid=threadlocal.auditlog['signal_duid'])
         return response
 
     def process_exception(self, request, exception):
         """
         Disconnects the signal receiver to prevent it from staying active in case of an exception.
         """
-        if hasattr(request, 'auditlog_ts'):
-            pre_save.disconnect(sender=LogEntry, dispatch_uid=(self.__class__, request.auditlog_ts))
+        if hasattr(threadlocal, 'auditlog'):
+            pre_save.disconnect(sender=LogEntry, dispatch_uid=threadlocal.auditlog['signal_duid'])
 
         return None
 
     @staticmethod
-    def set_actor(user, sender, instance, **kwargs):
+    def set_actor(user, sender, instance, signal_duid, **kwargs):
         """
         Signal receiver with an extra, required 'user' kwarg. This method becomes a real (valid) signal receiver when
         it is curried with the actor.
         """
+        if signal_duid != threadlocal.auditlog['signal_duid']:
+            return
         try:
             app_label, model_name = settings.AUTH_USER_MODEL.split('.')
             auth_user_model = get_model(app_label, model_name)
