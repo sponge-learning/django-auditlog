cc0382468dd4fb043624651c5881f627f89024ef
vvm46Vasylyk <volodumyr.vasylyk@spongelearning.com>
2023-06-20 15:50:15 +0200

    db_constraint equal False for actor field

--- src/auditlog/models.py
+++ src/auditlog/models.py
@@ -90,7 +90,7 @@ class LogEntry(models.Model):
     object_repr = models.TextField(verbose_name=_("object representation"))
     action = models.PositiveSmallIntegerField(choices=Action.choices, verbose_name=_("action"))
     changes = models.TextField(blank=True, verbose_name=_("change message"))
-    actor = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL, related_name='+', verbose_name=_("actor"))
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL, related_name='+', verbose_name=_("actor"), db_constraint=False)
     timestamp = models.DateTimeField(auto_now_add=True, verbose_name=_("timestamp"))
 
     objects = LogEntryManager()
