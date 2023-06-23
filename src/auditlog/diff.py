de60ccb19b6728a158f82f1b11cf08643d10d6c7
David Bramwell <david.bramwell@spongelearning.com>
2023-06-23 15:07:53 +0100

    SPRK-2269: Fix smart_text

--- src/auditlog/diff.py
+++ src/auditlog/diff.py
@@ -146,7 +146,7 @@ def model_instance_diff(old, new):
         new_value = get_field_value(new, field)
 
         if old_value != new_value:
-            diff[field.name] = (smart_text(old_value), smart_text(new_value))
+            diff[field.name] = (smart_str(old_value), smart_str(new_value))
 
     if len(diff) == 0:
         diff = None
